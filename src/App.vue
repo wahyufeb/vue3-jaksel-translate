1ac0342a9ba06b8dde1346722f9c3fd56c148f4e
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-16 23:15:48 +0700

    Add transition

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,27 @@ import { getCurrentTheme } from "@/composables/useThemeSwitcher";
 </template>
 
 <style>
-.fade-enter-active {
-  animation: coming 0.4s;
-  animation-delay: 0.2s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
 }
 
-.fade-leave-active {
-  animation: going 0.4s;
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateY(-20px);
+  opacity: 0;
 }
 </style>
