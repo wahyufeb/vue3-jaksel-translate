451c33e1d6f3fbadb821bc8b8d391d4bf059cdae
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-16 23:15:19 +0700

    Handle token Cookie

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { BASE_URL } from "@/config/environment";
+import { decryptCrypto } from "@/services/generic";
+import Cookies from "js-cookie";
 
 const AxiosInstance: AxiosInstance = axios.create({
   baseURL: BASE_URL,
@@ -8,5 +10,12 @@ const AxiosInstance: AxiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+const tokenCookie = Cookies.get("token");
+if (tokenCookie) {
+  AxiosInstance.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${decryptCrypto(tokenCookie)}`;
+}
 
 export default AxiosInstance;
