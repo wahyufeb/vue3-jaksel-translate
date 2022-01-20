f3aa69573cc7dc4c920f1f817b0e221f9aceda63
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-20 23:13:33 +0700

    Fix z-index

--- src/components/Modal/Modal.vue
+++ src/components/Modal/Modal.vue
@@ -25,7 +25,7 @@ const props = defineProps({
 <template>
   <div
     v-if="props.show"
-    class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+    class="main-modal fixed w-full h-100 inset-0 z-30 overflow-hidden flex justify-center items-center animated fadeIn faster"
     style="background: rgba(0, 0, 0, 0.7)"
   >
     <div
