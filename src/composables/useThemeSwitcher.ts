a3a74a39b1b86b57eb9ee8c115e8e7fcfc305283
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-01-24 16:47:29 +0700

    fix: composables

--- src/composables/useThemeSwitcher.ts
+++ src/composables/useThemeSwitcher.ts
@@ -10,15 +10,25 @@ export const toggleTheme = (): void => {
   }
 };
 
-export const getCurrentTheme = (): string => currentTheme.value;
+export const getCurrentTheme = (): string => {
+  handleBodyColor(currentTheme.value);
+  return currentTheme.value;
+};
 
 const setLightTheme = () => {
   currentTheme.value = "light";
   localStorage.setItem("theme", "light");
   document.documentElement.classList.remove("dark");
+  handleBodyColor("light");
 };
 const setDarkTheme = () => {
   currentTheme.value = "dark";
   localStorage.setItem("theme", "dark");
   document.documentElement.classList.add("dark");
+  handleBodyColor("dark");
+};
+
+const handleBodyColor = (theme: string) => {
+  document.body.style.backgroundColor =
+    theme === "dark" ? "#0D2438" : "#F7F8FC";
 };
